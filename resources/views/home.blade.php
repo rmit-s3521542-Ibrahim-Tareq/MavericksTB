67c748dab5b793d5f4731eae52b799d1780a488f
NICHOLAS <s3546815@student.rmit.edu.au>
2016-09-17 11:14:30 +1000

    fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,500i,700,700i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
     <link rel="stylesheet" href="../public/css/animate.css">
-    <link rel="stylesheet" href="../../../public/css/style.css">
+    <link rel="stylesheet" href="../public/css/style.css">
 	
 	<style>
 		.cinemaSession{
