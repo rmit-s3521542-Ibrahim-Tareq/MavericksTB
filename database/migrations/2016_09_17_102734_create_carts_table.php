2231ad8e2447c6a1fedf58e912312aacc402ff80
Gl3nda85 <s3249319@student.rmit.edu.au>
2016-09-18 13:01:53 +1000

    added database and migrations

---
+++ database/migrations/2016_09_17_102734_create_carts_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id');
+
+            $table->integer('session_time_id');
+
+
+
+            $table->timestamps();
+        });
+
+        Schema::table('carts',function ($table){
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+
+            //session time that was booked
+            $table->foreign('session_time_id')
+                ->references('id')
+                ->on('session_times');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('carts');
+    }
+}
