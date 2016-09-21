48253a682e334fe2ed91edcad71bb0115ab3a2f9
NICHOLAS <s3546815@student.rmit.edu.au>
2016-09-21 20:49:10 +1000

    merge avtar and nicholas

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movies;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,7 +10,8 @@ use App\Http\Requests;
 class MovieController extends Controller
 {
     public function showDefault() {
-        return view('movies');
+        $movies = Movies::all(['movie_name', 'genre', 'poster_url', 'imdb_rating']);
+        return view('movies', ['movies' => $movies]);
     }
 
 }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -10,21 +10,18 @@
     <hr/>
     <div class="tab-content clearfix">
         <div class="tab-pane nowshowingTab">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi explicabo placeat rerum sint ut! Accusantium aliquid amet consequatur dignissimos, distinctio eveniet ipsa laudantium molestiae placeat possimus, quae quod ullam voluptates?
+            @if(count($movies) < 1)
+            <p style="text-align:center;">There are no movies to display.</p>
+            @else
+                @foreach ($movies as $m)
+                <div class="col-lg-3 boxHits">
+                    <img src="{{url('/')}}/img/{{$m['poster_url']}}" class="img-thumbnail" alt="sully" width="290" height="200" />
+                </div>
+                @endforeach
+            @endif
         </div>
         <div class="tab-pane comingsoonTab">
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloremque doloribus enim facere
-                laborum mollitia placeat repudiandae rerum sunt voluptas? Ad commodi, debitis enim exercitationem fugiat
-                natus recusandae rerum vel.
-            </div>
-            <div>A accusantium animi asperiores aut autem eius, enim esse laborum nesciunt nobis non obcaecati
-                perspiciatis praesentium quas quos repellat sint velit veniam vitae voluptatibus. Beatae debitis ipsa
-                libero modi praesentium.
-            </div>
-            <div>Aut cumque esse neque possimus quibusdam repudiandae! Aspernatur assumenda, consequuntur culpa cum hic
-                impedit in, molestiae nobis nulla, optio pariatur praesentium quisquam temporibus! Placeat ratione
-                reprehenderit saepe sit tempore tenetur.
-            </div>
+
         </div>
     </div>
 </div>
