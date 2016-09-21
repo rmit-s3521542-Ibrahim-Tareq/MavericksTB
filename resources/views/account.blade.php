5a6261a9cb7d3bb89c7bfa4211ac47e42cc99dcf
avtar singh <s3530196@student.rmit.edu.au>
2016-09-21 20:40:36 +1000

    Merge branches 'avtar' and 'nicholas' of https://github.com/rmit-s3521542-Ibrahim-Tareq/MavericksTB into avtar
    # Conflicts:
    #	.env
    #	.idea/modules.xml
    #	.idea/workspace.xml

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -8,10 +8,103 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Hello Nick..
+                </div>
+                <div class="row">
+                    <div class="col-md-6 bookingHead"><h4><b>Bookings</b></h4><hr>
+
+
+
+
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingOne">
+                                    <h4 class="panel-title">
+                                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+
+                                                <div class="row">
+                                                    <div class="col-md-4">X-Men</div>
+                                                    <div class="col-md-4">28-12-2028</div>
+                                                    <div class="col-md-4">12:00AM</div>
+                                                </div>
+
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                    <div class="panel-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingTwo">
+                                    <h4 class="panel-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+
+                                                <div class="row">
+                                                    <div class="col-md-4">X-Men</div>
+                                                    <div class="col-md-4">28-12-2028</div>
+                                                    <div class="col-md-4">12:00AM</div>
+                                                </div>
+
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                    <div class="panel-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingThree">
+                                    <h4 class="panel-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+
+                                                <div class="row">
+                                                    <div class="col-md-4">X-Men</div>
+                                                    <div class="col-md-4">28-12-2028</div>
+                                                    <div class="col-md-4">12:00AM</div>
+                                                </div>
+
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                                    <div class="panel-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+                    </div>
+                    <div class="col-md-6 checkout"><h4><b>Change Password</b></h4><hr></div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('additionalCss')
+<style>
+    .bookingHead {
+        border-right: 1px solid #484747;
+        padding-left: 30px;
+        text-align: center;
+    }
+
+    .checkout{
+        text-align: center;
+    }
+</style>
 @endsection
