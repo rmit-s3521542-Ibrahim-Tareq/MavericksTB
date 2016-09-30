4d2783264823e15b7cd76a68d867982b44854d3c
Gl3nda85 <s3249319@student.rmit.edu.au>
2016-09-30 12:23:21 +1000

    Merge branch 'dylan' of https://github.com/rmit-s3521542-Ibrahim-Tareq/MavericksTB into dylan
    # Conflicts:
    #	app/Http/routes.php

--- resources/views/components/global-modals.blade.php
+++ resources/views/components/global-modals.blade.php
@@ -1,106 +1,3 @@
-
-<div id="cinemaModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Cinema Search</h4>
-            </div>
-
-            <div class="modal-body">
-                <form class="form-horizontal" role="form" id="ticketform">
-                    <div class="form-group">
-                        <label class="control-label col-sm-3" for="subject"><p>Select Cinema:</p></label>
-                        <div class="col-sm-9">
-                            <select class="form-control input-sm" id="os" name="os">
-                                <option selected disabled hidden style='display: none' value=''></option>
-                                <option>Melbourne Central</option>
-                                <option>Watergardens</option>
-                                <option>Northlands</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-3" for="subject"><p>Select Movie:</p></label>
-                        <div class="col-sm-9">
-                            <select class="form-control input-sm" id="os" name="os">
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-3" for="subject"><p>Select Session:</p></label>
-                        <div class="col-sm-9">
-                            <select class="form-control input-sm" id="os" name="os">
-                                <option selected disabled hidden style='display: none' value=''></option>
-                                <option>Monday, 2-5pm</option>
-                                <option>Wednesday, 5-7pm</option>
-                                <option>Friday, 8-10pm</option>
-                            </select>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Buy Tickets</button>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-<div id="movieModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Movie Search</h4>
-            </div>
-            <div class="modal-body">
-                <form class="form-horizontal" role="form" id="ticketform">
-                    <div class="form-group">
-                        <label class="control-label col-sm-3" for="subject"><p>Select Movie:</p></label>
-                        <div class="col-sm-9">
-                            <select class="form-control input-sm" id="os" name="os">
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-3" for="subject"><p>Select Cinema:</p></label>
-                        <div class="col-sm-9">
-                            <select class="form-control input-sm" id="os" name="os">
-                                <option selected disabled hidden style='display: none' value=''></option>
-                                <option>Melbourne Central</option>
-                                <option>Watergardens</option>
-                                <option>Northlands</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-3" for="subject"><p>Select Session:</p></label>
-                        <div class="col-sm-9">
-                            <select class="form-control input-sm" id="os" name="os">
-                                <option selected disabled hidden style='display: none' value=''></option>
-                                <option>Monday, 2-5pm</option>
-                                <option>Wednesday, 5-7pm</option>
-                                <option>Friday, 8-10pm</option>
-                            </select>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Buy Tickets</button>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-
 <div class="overlay" id="movieOverlay">
     <div class="imgOverlay"></div>
     <div class="box">
@@ -164,22 +61,22 @@
                                         <tbody>
                                         <tr>
                                             <td>Child</td>
-                                            <td><input type="number" value="0" name="childticket" min="0" max="10" class="numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
+                                            <td><input type="number" value="0" name="childticket" min="0" max="10" class="form-control numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
                                             <td>$<span class="subtotalPrice resetMe">0.00</span></td>
                                         </tr>
                                         <tr>
                                             <td>Adult</td>
-                                            <td><input type="number" value="0" name="adulticket" min="0" max="10" class="numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
+                                            <td><input type="number" value="0" name="adulticket" min="0" max="10" class="form-control numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
                                             <td>$<span class="subtotalPrice resetMe">0.00</span></td>
                                         </tr>
                                         <tr>
                                             <td>Seniors</td>
-                                            <td><input type="number" value="0" name="seniorticket" min="0" max="10" class="numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
+                                            <td><input type="number" value="0" name="seniorticket" min="0" max="10" class="form-control numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
                                             <td>$<span class="subtotalPrice resetMe">0.00</span></td>
                                         </tr>
                                         <tr>
                                             <td>Concession</td>
-                                            <td><input type="number" value="0" name="concessionticket" min="0" max="10" class="numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
+                                            <td><input type="number" value="0" name="concessionticket" min="0" max="10" class="form-control numOfTickets resetMe" onchange="priceValidator(this);calculatePrice()" onkeyup="this.value=this.value.replace(/[^\d]/g,'')"></td>
                                             <td>$<span class="subtotalPrice resetMe">0.00</span></td>
                                         </tr>
                                         </tbody>
