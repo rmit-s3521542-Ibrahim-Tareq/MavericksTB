de62d73d905be4b2951073324ff658b67ae8d74b
avtar singh <s3530196@student.rmit.edu.au>
2016-09-30 14:04:01 +1000

    search algorithm implemented..lol

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SessionTimes;
+use App\Models\Cinemas;
+use App\Models\Movies;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Input;
@@ -11,10 +14,19 @@ class SearchController extends Controller
 {
     public function process() {
         $term = Input::get('term');
-        return Redirect::to('search/'.$term) ;
-    }
-
-    public function search($term) {
-        return view('search', ['term' => $term]);
+        $arr = array();
+        $cinemaID = Cinemas::where('cinema_name', '=', $term)->first(['id']);
+        if(count($cinemaID)>0) {
+            $sessionID = SessionTimes::where('cinema_id', '=', $cinemaID['id'])->get(['movie_id']);
+            $sessionID= $sessionID->unique('movie_id');
+            foreach ($sessionID as $s) {
+                $movieDetails = Movies::where('id', '=', $s['movie_id'])->first(['id', 'movie_name', 'poster_url']);
+                array_push($arr, $movieDetails);
+            }
+        }
+        else{
+            $arr = Movies::where('movie_name', '=', $term)->get(['id', 'movie_name', 'poster_url']);
+        }
+        return view('search', ['term' => $term, 'movies' => $arr]);
     }
 }
