25fdef7798816255cac210c4d8904f8ee40131ee
NICHOLAS <s3546815@student.rmit.edu.au>
2016-10-15 21:25:56 +1100

    unit tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,26 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
+    /* Check if the links in the navbar is working */
+    public function testNavigation()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+            ->click('Cart')
+            ->seePageIs('/cart');
+    }
+
+    /* Check if the ticket info is successfully submitted to the cart page */
+    public function testSubmitToCart()
+    {
+        $this->visit('/movies')
+            ->select('2', 'childticket')
+            ->press('Add to cart')
+            ->seePageIs('/movies');
+    }
+
+    /* Check if the database is working properly */
+    public function testDatabase()
+    {
+        $this->seeInDatabase('users', ['email' => 'zuc0001@live.com']);
     }
 }
